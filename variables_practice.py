c860e1de914e04fb6828483848416469a717d1a2
valeriesiu <valerie.siu@ucas-edu.net>
2026-09-10 14:49:22 -0600

    variables practice fixed

--- variables_practice.py
+++ variables_practice.py
@@ -1,19 +1,19 @@
 # VS, variables practice
 
-name =("valerie")
-number1to10 =("5")
-number1to1000 = ("72")
-breakfast = ("eggs")
-color = ("light pink")
-school = ("maeser")
-year = ("2026")
-eyecolor = ("browm")
-age = ("14")
-subject = ("english")
+name ="valerie"
+number1to10 ="5"
+number100to1000 = "789"
+breakfast = "eggs"
+color = "light pink"
+school = "maeser"
+year = "2026"
+eyecolor = "browm"
+age = "14"
+subject = "english"
 
 print("My name is", name)
 print("A number from 1 to 10 is", number1to10)
-print("A number from 1 to 1000 is" ,number1to1000)
+print("A number from 1 to 1000 is" ,number100to1000)
 print("Today I had", breakfast)
 print("My favorite color is", color)
 print("I previously went to",school)
