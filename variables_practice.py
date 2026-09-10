838bb3d980db89a9abbf8f46fa76da4a0ed2a22c
valeriesiu <valerie.siu@ucas-edu.net>
2026-09-10 14:44:10 -0600

    variables practice

---
+++ variables_practice.py
@@ -0,0 +1,23 @@
+# VS, variables practice
+
+name =("valerie")
+number1to10 =("5")
+number1to1000 = ("72")
+breakfast = ("eggs")
+color = ("light pink")
+school = ("maeser")
+year = ("2026")
+eyecolor = ("browm")
+age = ("14")
+subject = ("english")
+
+print("My name is", name)
+print("A number from 1 to 10 is", number1to10)
+print("A number from 1 to 1000 is" ,number1to1000)
+print("Today I had", breakfast)
+print("My favorite color is", color)
+print("I previously went to",school)
+print("The year is",year)
+print("My eyes are", eyecolor)
+print("My age is", age)
+print("my favorite subject is", subject)
